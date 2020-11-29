c105a42b631eea02d2d1cc0140140750da5dc715
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-11-29 16:52:23 +0300

    add CardValidator.js

---
+++ src/js/CardValidator.js
@@ -0,0 +1,17 @@
+export default class CardValidator {
+  static luhnAlgorithm(cardNumber) {
+    let digitSum = 0;
+
+    for (let i = 0; i < cardNumber.length; i += 1) {
+      let digit = parseInt(cardNumber[i], 10);
+      if ((cardNumber.length - i) % 2 === 0) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      digitSum += digit;
+    }
+    return digitSum % 10 === 0;
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import CardValidator from './CardValidator';
+
+// console.log(CardValidator.luhnAlgorithm('6011649668448648115'));
